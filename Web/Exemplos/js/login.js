a1d295be976c2bff53e1d282970d9caa91f4c3c1
Erich Müller Dutra <erichmdutra@gmail.com>
2021-10-16 21:20:37 -0300

    correção secundária para separação das branches

--- Web/Exemplos/js/login.js
+++
@@ -1,9 +0,0 @@
-const btnMoble = document.getElementById('btn-mobile');
-
-function toggleMenu(event) {
-    if (event.type == 'touchstart') event.preventDefault()
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active')
-}
-
-btnMoble.addEventListener('click', toggleMenu);
